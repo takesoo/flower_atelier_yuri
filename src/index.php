af5a0adda6cc2b3479dcd0327757737cee9b79e0
hotaka kubota <>
2020-05-16 19:29:23 +0900

    stylesheet全部読み込むように変更

--- src/index.php
+++ src/index.php
@@ -4,8 +4,19 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>FlowerAterierYuri</title>
-  <?php $uri = $_SERVER["REQUEST_URI"]; ?>
+  <!-- <?php $uri = $_SERVER["REQUEST_URI"]; ?> -->
   <link rel="stylesheet" href="css/<?php echo $uri; ?>.css">
+  <link rel="stylesheet" href="css/kessai.css">
+  <link rel="stylesheet" href="css/kyoutu.css">
+  <link rel="stylesheet" href="css/mail_magazine.css">
+  <link rel="stylesheet" href="css/my_account.css">
+  <link rel="stylesheet" href="css/option.css">
+  <link rel="stylesheet" href="css/otoiawase.css">
+  <link rel="stylesheet" href="css/syouhin_ichiran.css">
+  <link rel="stylesheet" href="css/syouhin_syousai.css">
+  <link rel="stylesheet" href="css/tokutei_syou_torihiki.css">
+  <link rel="stylesheet" href="css/tomodachi.css">
+  <link rel="stylesheet" href="top.css">
 </head>
 <body>
   <!-- <?php echo $uri; ?> -->
